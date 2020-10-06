5e91d7af7db2c1e97ff9afbfc979d5593f722384
Aditya shukla <64006149+adityashukla0801@users.noreply.github.com>
2020-10-06 14:23:25 +0530

    Add files via upload

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ icons, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h2>
+        <i className={icons} />
+        {title}
+      </h2>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icons: 'fab fa-github',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icons: PropTypes.string.isRequired,
+};
+export default Navbar;
